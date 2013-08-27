5259253e46067d93340c2e4d93d99393e67153e7
Jeffrey Meyers <meyers2@pdx.edu>
2013-08-27 16:15:12 -0700

    using osmosis to run highway filter and create change file which is then used to create osm file of changes viewable in josm

--- change/change2osm.py
+++ change/change2osm.py
@@ -2,17 +2,9 @@ import urllib2
 from xml.etree import cElementTree as ElementTree
 from xml.etree.cElementTree import Element
 
-old_path = '/home/jeff/trimet/test/full.osm'
-change_path = '/home/jeff/trimet/test/change.osm'
-
-old_tree = ElementTree.parse(old_path)
-old_root = old_tree.getroot()
-
-delete_node = {}
-delete_way = {}
-delete_relation = {}
-
-
+old_path = '/home/jeff/trimet/osm/change/0813_highways.osm'
+change_path = '/home/jeff/trimet/osm/change/0813_to_0827_change.osm'
+out_path = '/home/jeff/trimet/osm/change/0813_to_0827.osm'
 
 def Identify(change_path):
     change_tree = ElementTree.parse(change_path)
@@ -27,24 +19,28 @@ def Identify(change_path):
     create_nodes = {}
     create_ways = {}
     create_relations = {}
-
+    needed_nodes = {}
     
     print "...delete"
     for child in change_root.findall('delete'):
         print 'node'
         for node in child.findall('node'):
 	    print node.attrib['id']
-	    delete_nodes[node.attrib['id']] = node.attrib['id']
+            node.append(Element('tag', {'k':'change', 'v':'delete'}))
+	    delete_nodes[node.attrib['id']] = node
         #way
         print 'way'
         for way in child.findall('way'):
 	    print way.attrib['id']
-	    delete_ways[way.attrib['id']] = way.attrib['id']
+	    way.append(Element('tag', {'k':'change', 'v':'delete'}))
+            for sub_element in way.findall('nd'):
+                needed_nodes[sub_element.attrib['ref']] = sub_element.attrib['ref']
+            delete_ways[way.attrib['id']] = way
 
         #relation
-        for relation in child.findall('relation'):
-	    print relation.attrib['id']
-	    delete_relations[relation.attrib['id']] = relation.attrib['id']
+        #for relation in child.findall('relation'):
+	#    print relation.attrib['id']
+	#    delete_relations[relation.attrib['id']] = relation.attrib['id']
 
     print "...modify"
     for child in change_root.findall('modify'):
@@ -53,17 +49,21 @@ def Identify(change_path):
         print 'node'
         for node in child.findall('node'):
 	    print node.attrib['id']
+            node.append(Element('tag', {'k':'change', 'v':'modify'}))
 	    modify_nodes[node.attrib['id']] = node
         #way
         print 'way'
         for way in child.findall('way'):
 	    print way.attrib['id']
+            way.append(Element('tag', {'k':'change', 'v':'modify'}))
+            for sub_element in way.findall('nd'):
+                needed_nodes[sub_element.attrib['ref']] = sub_element.attrib['ref']
 	    modify_ways[way.attrib['id']] = way
 
         #relation
-        for relation in child.findall('relation'):
-	    print relation.attrib['id']
-	    modify_relations[relation.attrib['id']] = relation
+        #for relation in child.findall('relation'):
+	#    print relation.attrib['id']
+	#    modify_relations[relation.attrib['id']] = relation
 
     print "...create"
     for child in change_root.findall('create'):
@@ -72,17 +72,21 @@ def Identify(change_path):
         print 'node'
         for node in child.findall('node'):
 	    print node.attrib['id']
-	    create_nodes[node.attrib['id']] = node
+	    node.append(Element('tag', {'k':'change', 'v':'create'}))
+            create_nodes[node.attrib['id']] = node
         #way
         print 'way'
         for way in child.findall('way'):
 	    print way.attrib['id']
-	    create_ways[way.attrib['id']] = way
+	    way.append(Element('tag', {'k':'change', 'v':'create'}))
+            for sub_element in way.findall('nd'):
+                needed_nodes[sub_element.attrib['ref']] = sub_element.attrib['ref']
+            create_ways[way.attrib['id']] = way
 
         #relation
-        for relation in child.findall('relation'):
-	    print relation.attrib['id']
-	    create_relations[relation.attrib['id']] = relation
+        #for relation in child.findall('relation'):
+	#    print relation.attrib['id']
+	#    create_relations[relation.attrib['id']] = relation
 
     return {'delete_nodes':delete_nodes, \
             'delete_ways':delete_ways, \
@@ -92,9 +96,59 @@ def Identify(change_path):
             'modify_relations':modify_relations, \
             'create_nodes':create_nodes, \
             'create_ways':create_ways, \
-            'create_relations':create_relations}
+            'create_relations':create_relations, \
+            'needed_nodes':needed_nodes}
+
+def New_Tree(old_root):
+    new_root = Element('osm')
+    new_root.set('version', old_root.attrib['version'])
+    new_root.append(old_root.find('bounds'))
+    return ElementTree.ElementTree(new_root)
+
+
+def Build(results, old_path, out_path):
+    old_tree = ElementTree.parse(old_path)
+    old_root = old_tree.getroot()
+    new_tree = New_Tree(old_root)
+    new_root = new_tree.getroot()
+
+    delete_nodes = results['delete_nodes']
+    delete_ways = results['delete_ways']
+    #delete_relations = results['delete_relations']
+    modify_nodes = results['modify_nodes']
+    modify_ways = results['modify_ways']
+    #modify_relations = results['modify_relations']
+    create_nodes = results['create_nodes']
+    create_ways = results['create_ways']
+    #create_relations = results['create_relations']
+    needed_nodes = results['needed_nodes']
+
+
+    for node in old_root.findall('node'):
+        match = node.attrib['id']
+        if match in delete_nodes:
+            new_root.append(delete_nodes[match])
+        elif match in modify_nodes:
+            new_root.append(modify_nodes[match])
+        elif match in create_nodes:
+            new_root.append(create_nodes[match])
+        elif match in needed_nodes:
+            new_root.append(node)
+
+
+    for way in old_root.findall('way'):
+        match = way.attrib['id']
+        if match in delete_ways:
+            new_root.append(delete_ways[match])
+        elif match in modify_ways:
+            new_root.append(modify_ways[match])
+        elif match in create_ways:
+            new_root.append(create_ways[match])
+
+
+    new_tree.write(out_path)
 
 
 
-Identify(change_path)
-
+results = Identify(change_path)
+Build(results, old_path, out_path)
---
+++ change/extract.py
@@ -0,0 +1,46 @@
+import subprocess
+
+root_path = '/home/jeff/trimet/oms/change/'
+recent = '08272013_multnomah-SE.osm'
+old = '08132013_multnomah-SE.osm'
+change_file = 'change.osm'
+
+"""
+filter out only ways that have a highway tag
+
+osmosis \  
+--read-xml 08132013_multnomah-SE.osm \
+--tag-filter accept-ways highway=* \
+--used-node \                        
+--write-xml 0813_highways.osm \        
+
+osmosis \                        
+--read-xml 08272013_multnomah-SE.osm \
+--tag-filter accept-ways highway=* \
+--used-node \                        
+--write-xml 0827_highways.osm \        
+
+
+derive change file from old and new highway filtered osm files
+
+osmosis                            
+--read-xml 0813_highways.osm \   
+--read-xml 0827_highways.osm \
+--derive-change \ 
+--write-xml-change 0813_to_0827_change.osm \       
+"""
+
+highways_old = 'osmosis --read-xml 08132013_multnomah-SE.osm --tag-filter accept-ways highway=* --used-node --tag-filter reject-relations --write-xml 0813_highways.osm'
+
+highways_new = 'osmosis --read-xml 08272013_multnomah-SE.osm --tag-filter accept-ways highway=* --used-node --tag-filter reject-relations --write-xml 0827_highways.osm'
+
+change = 'osmosis --read-xml 0813_highways.osm --read-xml 0827_highways.osm --derive-change --write-xml-change 0813_to_0827_change.osm'
+
+subprocess.call(highways_old, shell=True)
+subprocess.call(highways_new, shell=True)
+subprocess.call(change, shell=True)
+
+
+
+
+
