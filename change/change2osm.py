88571ecdba0700949a94d5fdd00bd525d88ce1bb
Jeffrey Meyers <meyers2@pdx.edu>
2013-08-29 11:38:27 -0700

    update

--- change/change2osm.py
+++ change/change2osm.py
@@ -1,13 +1,23 @@
 import urllib2
+import sys
 from xml.etree import cElementTree as ElementTree
 from xml.etree.cElementTree import Element
 
-old_path = '/home/jeff/trimet/osm/change/0813_highways.osm'
-change_path = '/home/jeff/trimet/osm/change/0813_to_0827_change.osm'
-out_path = '/home/jeff/trimet/osm/change/0813_to_0827.osm'
+#old_path = '/home/jeff/trimet/osm/change/0813_highways.osm'
+#change_path = '/home/jeff/trimet/osm/change/0813_to_0827_change.osm'
+#out_path = '/home/jeff/trimet/osm/change/0813_to_0827.osm'
 
-def Identify(change_path):
-    change_tree = ElementTree.parse(change_path)
+
+old_file = sys.argv[1]
+change_file = sys.argv[2]
+out_file = sys.argv[3]
+
+print old_file
+print change_file
+print out_file
+
+def Identify(change_file):
+    change_tree = ElementTree.parse(change_file)
     change_root = change_tree.getroot()
 
     delete_nodes = {}
@@ -31,8 +41,11 @@ def Identify(change_path):
             for sub_element in way.findall('nd'):
                 needed_nodes[sub_element.attrib['ref']] = sub_element.attrib['ref']
             delete_ways[way.attrib['id']] = way
-            if(way.attrib['id'] == '128573258'):
-                print "found delete way"
+
+        for relation in child.findall('relation'):
+	    relation.append(Element('tag', {'k':'change', 'v':'delete'}))
+            delete_relations[relation.attrib['id']] = relation
+
 
     for child in change_root.findall('modify'):
         for node in child.findall('node'):
@@ -45,24 +58,35 @@ def Identify(change_path):
                 needed_nodes[sub_element.attrib['ref']] = sub_element.attrib['ref']
 	    modify_ways[way.attrib['id']] = way
 
+        for relation in child.findall('relation'):
+	    relation.append(Element('tag', {'k':'change', 'v':'modify'}))
+            modify_relations[relation.attrib['id']] = relation
+
+
     for child in change_root.findall('create'):
         for node in child.findall('node'):
 	    node.append(Element('tag', {'k':'change', 'v':'create'}))
             create_nodes[node.attrib['id']] = node
+
         for way in child.findall('way'):
 	    way.append(Element('tag', {'k':'change', 'v':'create'}))
             for sub_element in way.findall('nd'):
                 needed_nodes[sub_element.attrib['ref']] = sub_element.attrib['ref']
             create_ways[way.attrib['id']] = way
-            if(way.attrib['id'] == '233469174'):
-                print "found create way"
+        
+        for relation in child.findall('relation'):
+	    relation.append(Element('tag', {'k':'change', 'v':'create'}))
+            create_relations[relation.attrib['id']] = relation
 
     return {'delete_nodes':delete_nodes, \
             'delete_ways':delete_ways, \
+            'delete_relations':delete_relations, \
             'modify_nodes':modify_nodes, \
             'modify_ways':modify_ways, \
+            'modify_relations':modify_relations, \
             'create_nodes':create_nodes, \
             'create_ways':create_ways, \
+            'create_relations':create_relations, \
             'needed_nodes':needed_nodes}
 
 def New_Tree(old_root):
@@ -72,8 +96,8 @@ def New_Tree(old_root):
     return ElementTree.ElementTree(new_root)
 
 
-def Build(results, old_path, out_path):
-    old_tree = ElementTree.parse(old_path)
+def Build(results, old_file, out_file):
+    old_tree = ElementTree.parse(old_file)
     old_root = old_tree.getroot()
     new_tree = New_Tree(old_root)
     new_root = new_tree.getroot()
@@ -110,9 +134,11 @@ def Build(results, old_path, out_path):
             new_root.append(create_ways[match])
 
 
-    new_tree.write(out_path)
+    new_tree.write(out_file)
 
 
 
-results = Identify(change_path)
-Build(results, old_path, out_path)
+results = Identify(change_file)
+Build(results, old_file, out_file)
+
+
--- change/extract.py
+++ change/extract.py
@@ -1,9 +1,9 @@
 import subprocess
 
-root_path = '/home/jeff/trimet/oms/change/'
-recent = '08272013_multnomah-SE.osm'
-old = '08132013_multnomah-SE.osm'
-change_file = 'change.osm'
+#root_path = '/home/jeff/trimet/oms/change/'
+#recent = '08272013_multnomah-SE.osm'
+#old = '08132013_multnomah-SE.osm'
+#change_file = 'change.osm'
 
 """
 filter out only ways that have a highway tag
@@ -30,16 +30,45 @@ osmosis
 --write-xml-change 0813_to_0827_change.osm \       
 """
 
-highways_old = 'osmosis --read-xml 08132013_multnomah.osm --tag-filter accept-ways highway=* --used-node --tag-filter reject-relations --write-xml 0813_highways.osm'
+osmosis = 'osmosis'
+read_xml = '--rx'
+write_xml = '--wx'
+derive_change = '--dc'
+write_change = '--wxc'
+tagfilter_highways = '--tf accept-ways highway=*'
+tagfilter_relations = '--tf accept-relations type=route,type=restriction'
+used_node = '--un'
 
-highways_new = 'osmosis --read-xml 08272013_multnomah.osm --tag-filter accept-ways highway=* --used-node --tag-filter reject-relations --write-xml 0827_highways.osm'
+old_osm = '08132013_multnomah.osm'
+new_osm = '08272013_multnomah.osm'
+old_highways = '0813_highways.osm'
+new_highways = '0827_highways.osm'
+change = '0813_to_0827_change.osm'
+final = '0813_to_0827.osm'
 
-change = 'osmosis --read-xml 0827_highways.osm --read-xml 0813_highways.osm --derive-change --write-xml-change 0813_to_0827_change.osm'
+process_old = " ".join([osmosis, read_xml, old_osm, tagfilter_highways, \
+                        used_node, tagfilter_relations, write_xml, old_highways])
 
-subprocess.call(highways_old, shell=True)
-subprocess.call(highways_new, shell=True)
-subprocess.call(change, shell=True)
+process_new = " ".join([osmosis, read_xml, new_osm, tagfilter_highways, \
+                        used_node, tagfilter_relations, write_xml, new_highways])
 
+
+process_change = " ".join([osmosis, read_xml, new_highways, read_xml, \
+                            old_highways, derive_change, write_change, change])
+
+process_create = " ".join(["python", "change2osm.py", old_highways, change, final])
+
+
+#highways_old = 'osmosis --read-xml 08132013_multnomah.osm --tag-filter accept-ways highway=* --used-node --tag-filter reject-relations --write-xml 0813_highways.osm'
+
+#highways_new = 'osmosis --read-xml 08272013_multnomah.osm --tag-filter accept-ways highway=* --used-node --tag-filter reject-relations --write-xml 0827_highways.osm'
+
+#change = 'osmosis --read-xml 0827_highways.osm --read-xml 0813_highways.osm --derive-change --write-xml-change 0813_to_0827_change.osm'
+
+subprocess.call(process_old, shell=True)
+subprocess.call(process_new, shell=True)
+subprocess.call(process_change, shell=True)
+subprocess.call(process_create, shell=True)
 
 
 
